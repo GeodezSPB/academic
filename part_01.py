42ab4f107eec4f8b1ad3455c449ea1ae23300e20
GeodezSPB <40434806+GeodezSPB@users.noreply.github.com>
2020-11-24 20:45:46 +0300

    First commit, drafts.
    Первый реппозиторий, мои черновики, тестируем гитхаб десктоп.

---
+++ part_01.py
@@ -0,0 +1,20 @@
+import math
+
+
+name = "Eugene"
+print("Hello World", name)
+year = 2020
+print("Today is", year, "year.")
+a=5; b=3
+r = a + b
+print ("a =", a,"b =", b, ": a + b =", r)
+if ( a>b ):
+    print ("a>b")
+else:
+    print ("a<=b")
+print("Чай")
+text = input("введите текст:")
+print("текст =", text)
+pi = math.acos(-1)
+print("Число ПИ =", pi)
+print(type(pi))
---
+++ part_03.py
@@ -0,0 +1,17 @@
+print("Задача №1")
+import math
+pi = math.acos(-1)
+print("Число ПИ =", pi)
+print("Задача №1.9")
+print("50")
+print("10")
+print("Задача №1.10")
+print("5\n10\n21")
+print("Задача №1.31а")
+a = 10; b = 25
+r = (a + b)/2
+print("Среднее арифметическое a", a, "и b", b, "=", r)
+print("Задача №1.31б")
+a = 10; b = 20
+r = math.sqrt(a * b)
+print("Среднее геометическое a", a, "и b", b, "=", r)
